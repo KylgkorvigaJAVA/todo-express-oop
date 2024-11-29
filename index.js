428f321cf64b5f5980d8c89f444af18995cd44ff
Kaspar <kaspar.viitkar@voco.ee>
2024-11-29 16:45:08 +0200

    Create new Todo from user data

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import todoRoutes from './routes/todos.js'
 
 const app = express()
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended: true}))
+
+app.use('/todos', todoRoutes)
 
 app.listen(3009, () =>{
     console.log('server is connected at port 3009')
 })
+
